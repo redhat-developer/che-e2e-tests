116029fc14fe2ef4528253ea843d016dddc126a1
kkanova <kkanova@redhat.com>
2018-11-01 15:42:52 +0100

    Add PVC check and creation. Refactor scripts.

--- mount-volume/run_test.sh
+++ mount-volume/run_test.sh
@@ -1,31 +1,73 @@
 #!/usr/bin/env bash
+
+source ./getopts_util.sh
+while getopts "hu:p:r:v:z:t:i:f" opt; do
+  case $opt in
+	h) printHelp
+       exit 0
+       ;;
+    u) export USERNAME=$OPTARG
+      ;;
+    p) export PASSWORD=$OPTARG
+      ;;
+    r) export URL=$OPTARG
+      ;;
+    v) export VOLUME_NAME=$OPTARG
+      ;;
+    z) export ZABBIX_PREFIX=$OPTARG
+      ;;
+    t) export ATTEMPT_TIMEOUT=$OPTARG
+      ;;
+    i) export ITERATIONS=$OPTARG
+      ;;
+    f) export FILL_PVC="true"
+      ;;
+    \?)
+      echo "\"$opt\" is an invalid option!"
+      exit 1
+      ;;
+    :)
+      echo "Option \"$opt\" needs an argument."
+      ;;
+  esac
+done
+checkParams
+
 chmod +x simple-pod.sh
 chmod +x init-env.sh
-set +e
 
-USERNAME=$1
-PASSWORD=$2
-URL=$3
-VOLUME_NAME=$4
-VOLUME_NAME=${VOLUME_NAME:-"claim-che-workspace"}
-ZABBIX_PREFIX=$5
-ATTEMPT_TIMEOUT=$6
-ATTEMPT_TIMEOUT=${ATTEMPT_TIMEOUT:-120}
-ITERATIONS=$7
-ITERATIONS=${ITERATIONS:-5}
 
 echo "Initializing environment"
 ./init-env.sh
 
+#login and choose right project
+chrlen=$((${#USERNAME}-3))
+echo "running tests with user: ${USERNAME:0:3} ${USERNAME:3:$chrlen}"
+oc login $URL -u $USERNAME -p $PASSWORD
+if [[ $? != 0 ]]; then 
+	echo "Can not login - please check credentials."
+	exit 1
+fi
+
+oc project "$USERNAME"
+
+echo "Ensure if PVC exists with required setup"
+if ./setup_pvc.sh; then 
+	echo -e "Setup completed. Continuing with tests.\\n"
+else
+	exit 1
+fi
+
 PATH=$PATH:$(pwd)/
 
-echo "Running tests"
-./simple-pod.sh $USERNAME $PASSWORD $URL $VOLUME_NAME $ATTEMPT_TIMEOUT $ITERATIONS
-RESULT=$?
-if [ $RESULT == 0 ]; then
-  ./zabbix.sh $URL $ZABBIX_PREFIX $ITERATIONS
+echo "   RUNNING TESTS"
+source ./simple-pod.sh
+simplePodRunTest 
+if [ $? == 0 ]; then
+  ./zabbix.sh "$URL" "$ZABBIX_PREFIX" "$ITERATIONS"
+  echo "Tests status: SUCCESS"
 else
-  echo "Tests have failed. Not reporting data."
+  echo "Tests status: FAILED - not reporting data to zabbix."
 fi
 
 set -e
--- mount-volume/zabbix.sh
+++ mount-volume/zabbix.sh
@@ -29,18 +29,18 @@ function getMetrics {
     file=$1
     while IFS=, read -r time_taken
     do
-        processItem ${time_taken}
-    done < ${file}
+        processItem "$time_taken"
+	done < "$file"
     med=$(sed "$((count/2+1))q;d" tmp.csv)
 }
 
 function addMetricsToLogFile {
     PREFIX_CURRENT=$1
     text="$HOST $PREFIX_CURRENT"
-    echo "$text-median $ZABBIX_TIMESTAMP $med" >> ${logfile}
-    echo "$text-max $ZABBIX_TIMESTAMP $max" >> ${logfile}
-    echo "$text-min $ZABBIX_TIMESTAMP $min" >> ${logfile}
-    echo "$text-avg $ZABBIX_TIMESTAMP $((avg/count))" >> ${logfile}
+    echo "$text-median $ZABBIX_TIMESTAMP $med" >> "$logfile"
+    echo "$text-max $ZABBIX_TIMESTAMP $max" >> "$logfile"
+    echo "$text-min $ZABBIX_TIMESTAMP $min" >> "$logfile"
+    echo "$text-avg $ZABBIX_TIMESTAMP $((avg/count))" >> "$logfile"
     med=0
     max=0
     min=0
@@ -50,13 +50,13 @@ function addMetricsToLogFile {
 
 function generateLogs {
     METRICS=$1
-    sort -n ${METRICS}.csv > tmp.csv
-    getMetrics ${METRICS}.csv
-    if [ ${ITERATIONS} == 1 ]; then
+    sort -n "$METRICS".csv > tmp.csv
+    getMetrics "$METRICS".csv
+    if [ $ITERATIONS == 1 ]; then
       PREFIX_CURRENT="${PREFIX}$(echo ${METRICS,,})-actual"
       echo "actual: $med for ${PREFIX_CURRENT}"
       text="$HOST $PREFIX_CURRENT"
-      echo "$text $ZABBIX_TIMESTAMP $max" >> ${logfile}
+      echo "$text $ZABBIX_TIMESTAMP $max" >> "$logfile"
       med=0
       max=0
       min=0
@@ -65,7 +65,7 @@ function generateLogs {
     else
       echo "median: $med   minimum: $min   maximum: $max   average:$((avg/count))"
       PREFIX_CURRENT="${PREFIX}$(echo ${METRICS,,})"
-      addMetricsToLogFile ${PREFIX_CURRENT}
+      addMetricsToLogFile "$PREFIX_CURRENT"
     fi
 }
 
@@ -75,8 +75,8 @@ ZABBIX_PREFIX=$2
 ITERATIONS=$3
 logfile=zabbix.log
 
-if [ -f ${logfile} ]; then
-    rm ${logfile}
+if [ -f "$logfile" ]; then
+    rm "$logfile"
 fi
 
 PREFIX=${ZABBIX_PREFIX:-"mount_volume-"}
@@ -90,4 +90,4 @@ generateLogs Start
 generateLogs Stop
 
 echo "  Uploading report to zabbix...";
-zabbix_sender -vv -i zabbix.log -T -z ${ZABBIX_SERVER} -p ${ZABBIX_PORT};
+zabbix_sender -vv -i zabbix.log -T -z "$ZABBIX_SERVER" -p "$ZABBIX_PORT";
